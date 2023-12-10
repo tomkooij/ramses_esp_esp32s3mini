fd798c6fac7ece8994d54a332739234860a2581a
Peter Price <peteraprice@btinternet.com>
2023-12-10 16:17:23 +0000

    Include LEDs in build

--- main/main.c
+++ main/main.c
@@ -16,12 +16,15 @@
 #include "freertos/FreeRTOS.h"
 
 #include "esp_debug.h"
+#include "led.h"
+
 #include "radio.h"
 #include "host.h"
 
 void app_main(void)
 {
   esp_debug_init();
+  led_init();
 
   Radio_init( CONFIG_RADIO_CORE );
   Host_init( CONFIG_HOST_CORE );
