1fbaf5c132bab8758dff6e0449f8c66307db3eed
Peter Price <peteraprice@btinternet.com>
2023-12-12 17:20:38 +0000

    Improve speed of SPI by having driver control CSn. Remove last remnants of arduino logic.

--- components/cc1101/cc1101.c
+++ components/cc1101/cc1101.c
@@ -35,10 +35,10 @@ static spi_bus_config_t const cc_buscfg = {
 };
 
 static spi_device_interface_config_t const cc_devcfg = {
-  .clock_speed_hz = 250000, // SPI clock is 250 KHz!
+  .clock_speed_hz = 1000000, // SPI clock is 1 MHz!
   .queue_size = 7,
   .mode = 0, // SPI mode 0
-  .spics_io_num = -1, // we will use manual CS control
+  .spics_io_num = CONFIG_CC_CSN_GPIO,
   .flags = SPI_DEVICE_NO_DUMMY
 };
 
@@ -46,10 +46,6 @@ static spi_device_handle_t cc_handle;
 
 static void cc_spi_init() {
   esp_err_t ret;
-
-  gpio_reset_pin( CONFIG_CC_CSN_GPIO );
-  gpio_set_direction( CONFIG_CC_CSN_GPIO, GPIO_MODE_OUTPUT );
-  gpio_set_level( CONFIG_CC_CSN_GPIO, 1 );
 
   ret = spi_bus_initialize( CC_HOST_ID, &cc_buscfg, SPI_DMA_CH_AUTO );
   ESP_LOGI( TAG, "spi_bus_initialize=%d", ret );
@@ -60,29 +56,13 @@ static void cc_spi_init() {
   assert(ret==ESP_OK);
 }
 
-static uint8_t cc_spi_transfer( uint8_t address )
-{
-  uint8_t dataout = address;
-  uint8_t datain;
-
-  spi_transaction_t SPITransaction = {
-    .length = 8,
-	.tx_buffer = &dataout,
-	.rx_buffer = &datain
-  };
-
-  spi_device_transmit( cc_handle, &SPITransaction );
-
-  return datain;
-}
-
-static bool cc_spi_write_byte(uint8_t* Dataout, size_t DataLength )
+static bool spi_write_byte(uint8_t *status, uint8_t* Dataout, size_t DataLength )
 {
   if ( DataLength > 0 ) {
     spi_transaction_t SPITransaction = {
       .length = DataLength * 8,
 	  .tx_buffer = Dataout,
-	  .rx_buffer = NULL
+	  .rx_buffer = status
 	};
 
 	spi_device_transmit( cc_handle, &SPITransaction );
@@ -116,21 +96,11 @@ static bool spi_read_byte(uint8_t* Datain, uint8_t* Dataout, size_t DataLength )
 #define cc_assert()    gpio_set_level( CONFIG_CC_CSN_GPIO, LOW  )
 #define cc_deassert()  gpio_set_level( CONFIG_CC_CSN_GPIO, HIGH )
 
-#define cc_check_miso() gpio_get_level( CONFIG_CC_MISO_GPIO )
-#define cc_check_GDO0()	gpio_get_level( CONFIG_CC_GDO0_GPIO )
-#define cc_check_GDO2()	gpio_get_level( CONFIG_CC_GDO2_GPIO )
-
 static uint8_t cc_read( uint8_t addr ) {
+  uint8_t out = addr | CC_READ;
   uint8_t data ;
 
-  cc_assert();
-
-  while( cc_check_miso() );	// Wait MISO low
-
-  cc_spi_transfer ( addr | CC_READ );
-  data = cc_spi_transfer( 0 );
-
-  cc_deassert();
+  spi_read_byte( &data, &out, 1 );
 
   return data;
 }
@@ -138,14 +108,8 @@ static uint8_t cc_read( uint8_t addr ) {
 static uint8_t cc_write( uint8_t addr, uint8_t b ) {
   uint8_t result;
 
-  cc_assert();
-
-  while( cc_check_miso() );
-
-  cc_spi_transfer(addr);
-  result = cc_spi_transfer(b);
-
-  cc_deassert();
+  uint8_t out[2] = { addr, b };
+  spi_write_byte( &result, out, 2 );
 
   return result;
 }
@@ -154,12 +118,7 @@ uint8_t cc_strobe( uint8_t cmd )
 {
   uint8_t result;
 
-  cc_assert();
-  while( cc_check_miso() );
-
-  result = cc_spi_transfer( cmd );		// Send strobe command
-
-  cc_deassert();
+  spi_write_byte( &result, &cmd, 1 );
 
   return result;
 }
@@ -174,16 +133,10 @@ uint8_t cc_write_fifo(uint8_t b) {
  */
 
 void cc_enter_idle_mode(void) {
-//  EIMSK &= ~INT_MASK;            // Disable interrupts
-
   while ( CC_STATE( cc_strobe( CC_SIDLE ) ) != CC_STATE_IDLE );
-
-//  EIFR  |= INT_MASK;          // Acknowledge any  previous edges
 }
 
 void cc_enter_rx_mode(void) {
-//  EIMSK &= ~INT_MASK;            // Disable interrupts
-
   while ( CC_STATE( cc_strobe( CC_SIDLE ) ) != CC_STATE_IDLE ){}
 
   cc_write( CC_IOCFG0, 0x2E );      // GDO0 not needed
@@ -191,13 +144,9 @@ void cc_enter_rx_mode(void) {
 
   cc_strobe( CC_SFRX );
   while ( CC_STATE( cc_strobe( CC_SRX ) ) != CC_STATE_RX ){}
-
-//  EIFR  |= INT_MASK;          // Acknowledge any  previous edges
 }
 
 void cc_enter_tx_mode(void) {
-//  EIMSK &= ~INT_MASK;            // Disable interrupts
-
   while ( CC_STATE( cc_strobe( CC_SIDLE ) ) != CC_STATE_IDLE ){}
 
   cc_write( CC_PKTCTRL0, 0x02 );    // Fifo mode, infinite packet
@@ -205,8 +154,6 @@ void cc_enter_tx_mode(void) {
 
   cc_strobe( CC_SFTX );
   while ( CC_STATE( cc_strobe( CC_STX ) ) != CC_STATE_TX ){}
-
-//  EIFR  |= INT_MASK;          // Acknowledge any  previous edges
 }
 
 void cc_fifo_end(void) {
@@ -224,13 +171,10 @@ uint8_t cc_read_rssi(void) {
 /************************************************************
  * CC1101 initialisation
  */
-void cc_init(void) {
-  uint8_t param[CC_PARAM_MAX];
-  uint8_t i,len;
-
-  esp_log_level_set(TAG, CONFIG_CC_LOG_LEVEL );
-
-  cc_spi_init();
+static void cc_spi_reset(void) {
+  gpio_reset_pin( CONFIG_CC_CSN_GPIO );
+  gpio_set_direction( CONFIG_CC_CSN_GPIO, GPIO_MODE_OUTPUT );
+  gpio_set_level( CONFIG_CC_CSN_GPIO, 1 );
 
   cc_deassert();
   delayMicroseconds(1);
@@ -240,6 +184,16 @@ void cc_init(void) {
 
   cc_deassert();
   delayMicroseconds(41);
+}
+
+void cc_init(void) {
+  uint8_t param[CC_PARAM_MAX];
+  uint8_t i,len;
+
+  esp_log_level_set(TAG, CONFIG_CC_LOG_LEVEL );
+
+  cc_spi_reset();
+  cc_spi_init();
 
   cc_strobe(CC_SRES);
   //cc_strobe(CC_SCAL);
@@ -254,7 +208,6 @@ void cc_init(void) {
 
   cc_write( CC_FIFOTHR, ( param[CC_FIFOTHR]&0xF0 )+14 );	  // TX Fifo Threshold 5
 
-//  cc_enter_idle_mode();
   cc_enter_rx_mode();
 }
 
